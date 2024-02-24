b666d35b33db551e22b413d85e00dfb56c3bd8e4
samuelrenna <samuelrenna@gmail.com>
2024-02-24 20:55:51 -0300

    center img detail

--- src/Components/ItemDetail/ItemDetail.jsx
+++ src/Components/ItemDetail/ItemDetail.jsx
@@ -5,7 +5,7 @@ const ItemDetail = ({ name, img, categoryUno, categoryDos, description, price, s
 return (
     <div className="container d-flex justify-content-center align-items-center">
         <div className="card" style={{ width: '18rem' }}>
-        <img src={img} className="card-img-top" alt={name} style={{ width: '200px', height: '200px' }} />
+        <img src={img} className="card-img-top mx-auto" alt={name} style={{ width: '200px', height: '200px' }} />
             <div className="card-body">
         <h2 className="card-title">{name}</h2>
         <p className="card-text">Categoria: {categoryUno},{categoryDos}</p>
