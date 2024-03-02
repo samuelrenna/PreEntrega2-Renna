2da6299e71de53a9c1563883b2ae34b1d1063b6d
samuelrenna <samuelrenna@gmail.com>
2024-03-02 18:51:42 -0300

    componete carrito y arreglos del detail

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ import './App.css';
 import NavBar from './Components/Navbar/NavBar';
 import ItemListContainer from './Components/ItemListContainer/ItemListContainer';
 import ItemDetailContainer from './Components/ItemDetailContainer/ItemDetailContainer';
+import Cart from './Components/Cart/Cart';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Footer from "./Components/footer/Footer";
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
@@ -15,7 +17,8 @@ function App() {
           <Route path='/' element={<ItemListContainer greeting={"VERSUS GAMING"} />} />
           <Route path='/:categoryId' element={<ItemListContainer />} />
           <Route path='/item/:itemId' element={<ItemDetailContainer />} />
-        </Routes>
+          <Route path='/cart' element={<Cart />} />
+          </Routes>
         <Footer />
       </BrowserRouter>
     </div>
---
+++ src/Components/Cart/Cart.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Cart = () => {
+return (
+    <div>
+    <h2>Carrito</h2>
+    <p>Aquí irá el contenido del carrito...</p>
+    </div>
+);
+}
+
+export default Cart;
